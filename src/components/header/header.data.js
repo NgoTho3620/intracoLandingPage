6bb2c17e5637e2e54c4fd7974255041e95ef0be6
Tho Ngo <ngotho.3620@gmail.com>
2023-02-27 09:21:43 +0700

    first commit

---
+++ src/components/header/header.data.js
@@ -0,0 +1,42 @@
+export default [
+  // {
+  //   path: "home",
+  //   label: "Home",
+  // },
+  {
+    path: "gioi-thieu",
+    label: "Giới thiệu",
+  },
+  {
+    path: "dich-vu",
+    label: "Dịch vụ",
+  },
+  {
+    path: "viec-lam",
+    label: "Việc làm",
+  },
+  {
+    path: "tin-tuc",
+    label: "Tin tức",
+  },
+  {
+    path: "tai-lieu",
+    label: "Tài liệu",
+  },
+  {
+    path: "lien-he",
+    label: "Liên hệ",
+  },
+  // {
+  //   path: "feature",
+  //   label: "Features",
+  // },
+  // {
+  //   path: "pricing",
+  //   label: "Pricing",
+  // },
+  // {
+  //   path: "testimonial",
+  //   label: "Testimonial",
+  // },
+];
